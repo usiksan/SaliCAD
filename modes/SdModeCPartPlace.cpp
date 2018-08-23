729589a4ffb4069e0e6088461ab2620aa9ecbc8a
Sibilev Alexandr <usiksan@mail.ru>
2018-08-23 22:04:22 +0300

    Begin creation place mode.

--- modes/SdModeCPartPlace.cpp
+++ modes/SdModeCPartPlace.cpp
@@ -17,3 +17,104 @@ SdModeCPartPlace::SdModeCPartPlace(SdWEditorGraph *editor, SdProjectItem *obj) :
   {
 
   }
+
+
+void SdModeCPartPlace::activate()
+    {
+    }
+
+void SdModeCPartPlace::drawStatic(SdContext *ctx)
+    {
+    }
+
+void SdModeCPartPlace::drawDynamic(SdContext *ctx)
+    {
+    }
+
+int SdModeCPartPlace::getPropBarId() const
+    {
+    }
+
+void SdModeCPartPlace::propGetFromBar()
+    {
+    }
+
+void SdModeCPartPlace::propSetToBar()
+    {
+    }
+
+void SdModeCPartPlace::enterPoint(SdPoint)
+    {
+    }
+
+void SdModeCPartPlace::cancelPoint(SdPoint)
+    {
+    }
+
+void SdModeCPartPlace::movePoint(SdPoint)
+    {
+    }
+
+void SdModeCPartPlace::keyDown(int key, QChar ch)
+    {
+    }
+
+void SdModeCPartPlace::keyUp(int key, QChar ch)
+    {
+    }
+
+SdPoint SdModeCPartPlace::enterPrev()
+    {
+    }
+
+void SdModeCPartPlace::beginDrag(SdPoint)
+    {
+    }
+
+void SdModeCPartPlace::dragPoint(SdPoint)
+    {
+    }
+
+void SdModeCPartPlace::stopDrag(SdPoint)
+    {
+    }
+
+bool SdModeCPartPlace::getInfo(SdPoint p, QString &info)
+    {
+    }
+
+QString SdModeCPartPlace::getStepHelp() const
+    {
+    }
+
+QString SdModeCPartPlace::getModeThema() const
+    {
+    }
+
+QString SdModeCPartPlace::getStepThema() const
+    {
+    }
+
+int SdModeCPartPlace::getCursor() const
+  {
+  switch( getStep() ) {
+    //Is selection by rect
+    //[Происходит выделение прямоугольником]
+    case psmBeingSelRect : return CUR_SEL;
+    //No selection [Нет выделения]
+    case psmNoSelect :
+      //CheckPoint(prevMove);
+      //Позаботиться о строке состояния
+      //GetViewer()->SetMessage( GetStepHelp() );
+      return mBehindCursorTable.count() ? CUR_ TAKE_CUR : POINT_CUR;
+    default :
+    case psmMove : return PLACE_CUR;
+    }
+  }
+
+
+
+int SdModeCPartPlace::getIndex() const
+  {
+  return MD_PLACE;
+  }
--- modes/SdModeCPartPlace.h
+++ modes/SdModeCPartPlace.h
@@ -15,12 +15,88 @@ Description
 #define SDMODECPARTPLACE_H
 
 #include "SdModeCommon.h"
+#include "objects/SdContainer.h"
+
+//==============================================================================
+//--------------------- Режимы размещения --------------------------------------
+enum {             //Шаги режима размещения
+  psmNoSelect,     //Нет выделения
+  psmBeingSelRect, //Происходит выделение прямоугольником
+  psmMove,         //Перенос одного или нескольких элементов
+  psmLast };
+
+enum DPlaceModes {   //Способ выбора следующего компонента для размещения
+  dpmSmart,          //Разумный выбор из группы
+  dpmNextNumber,     //Следующий номер в классе элементов
+  dpmDialog,         //С помощью диалога
+  dpmSheet };        //Указанием в схеме
+
+enum DPlaceEndCode { //Коды завершения диалога выбора компонентов
+  placeCancel,       //Прекратить режим
+  placeSelect,       //Перейти к выбору компонента заданным способом
+  placeComp,         //Размещать компонент
+  placeGroup,        //Размещать группу компонентов по шаблону
+  placeSheet };      //Выбрать элементы для размещения из схемы
+
+struct DPlaceModeInfo {
+  //int             placeMode;  //Режим размещения
+  DString          prev;       //Предыдущий компонент
+  DName            shem;       //Предыдущая схема
+  DString          patSour;    //Список идентификаторов группы компонентов
+  DString          patDest;    //
+  DCompPlaceTable  table;      //Таблица компонентов
+  DPlatePic       *plate;      //Обрабатываемая плата
+  DWName           sheetName;  //Название схемы, из которой осуществляется выбор
+
+  DPlaceModeInfo() : table( 30, 30 ), plate(0) { }
+
+  void FindGroupBySour();
+  bool CheckMatch( int level, int maxLevel, int sour[], int target[] );
+  };
 
 
 class SdModeCPartPlace : public SdModeCommon
   {
+    SdPoint            mFirst;             //Начальная точка
+    SdPoint            mPrevMove;          //Предыдущая точка
+    SdProp             mLocalProp;         //Свойства выделенных объектов
+    SdSelector         mFragment;          //Набор выделенных объектов
+    SdObjectPtrList    mBehindCursorTable; //Список объектов под курсором
+    int                mBehindCursorIndex; //Индекс объекта среди объектов под курсором
+    bool               mShiftKey;
+    QString            mSmartName;         //Компонент, подлежащий размещению разумным режимом
+    SdGraphPartImp*    mBehindCursorPrt;   //Компонент, подлежащий выделению при нажатии левой кнопки мыши
+    bool               mInsertFlag;        //Автораздвижка компонентов
+    int                mBigCompPins;       //Количество выводов главного компонента
+    int                mBigCompIndex;      //Индекс главного компонента в таблице компонентов
+    //DLineProp         lineProp;          //Свойства для выделения прямоугольником
+    bool               mBySheet;           //Истина, когда производится выбор из листа
   public:
     SdModeCPartPlace( SdWEditorGraph *editor, SdProjectItem *obj );
+
+    // SdMode interface
+  public:
+    virtual void activate() override;
+    virtual void drawStatic(SdContext *ctx) override;
+    virtual void drawDynamic(SdContext *ctx) override;
+    virtual int getPropBarId() const override;
+    virtual void propGetFromBar() override;
+    virtual void propSetToBar() override;
+    virtual void enterPoint(SdPoint) override;
+    virtual void cancelPoint(SdPoint) override;
+    virtual void movePoint(SdPoint) override;
+    virtual void keyDown(int key, QChar ch) override;
+    virtual void keyUp(int key, QChar ch) override;
+    virtual SdPoint enterPrev() override;
+    virtual void beginDrag(SdPoint) override;
+    virtual void dragPoint(SdPoint) override;
+    virtual void stopDrag(SdPoint) override;
+    virtual bool getInfo(SdPoint p, QString &info) override;
+    virtual QString getStepHelp() const override;
+    virtual QString getModeThema() const override;
+    virtual QString getStepThema() const override;
+    virtual int getCursor() const override;
+    virtual int     getIndex() const override;
   };
 
 #endif // SDMODECPARTPLACE_H
