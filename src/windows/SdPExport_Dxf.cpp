b11735d3a5499795d7563f2d28318b250728c783
asibilev <asibilev@dialcomponent.ru>
2019-01-15 17:03:25 +0300

    Continue with DXF export.

--- src/windows/SdPExport_Dxf.cpp
+++ src/windows/SdPExport_Dxf.cpp
@@ -12,6 +12,29 @@ Description
   Creation dxf file from current object
 */
 #include "SdPExport_Dxf.h"
+#include "objects/SdContext.h"
+
+//Dxf context
+//With this context when we 'draw' picture we actualy not draw
+// Instead, we append items to dxf file
+struct SdDxfContext : public SdContext {
+    QString mBuffer;
+
+    SdDxfContext();
+    ~SdDxfContext() override { delete mPainter; }
+
+    // SdContext interface
+    virtual void setPen(int width, QColor color, int lineStyle) override;
+    virtual void fillRect(SdRect r) override;
+    virtual void circle(SdPoint center, int radius ) override;
+    virtual void circleFill(SdPoint center, int radius) override;
+    virtual void polygon(const SdPointList &points, const SdPolyWindowList &windows, SdLayer *layer) override;
+
+
+  };
+
+
+
 
 SdPExport_Dxf::SdPExport_Dxf(SdProjectItem *item, int step, SdPMasterList *list, QWidget *parent) :
   QWizardPage( parent ),
