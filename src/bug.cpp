51b316c742651a8ef4020fa81ead2e7ac1ae4e9e
asibilev <asibilev@dialcomponent.ru>
2019-02-11 18:00:06 +0300

    Some libraries modified.

--- src/bug.cpp
+++ src/bug.cpp
@@ -25,6 +25,8 @@
 
 //TODO B065 When upgrading component don't changed ident (and perhaps - value)
 
+//TODO B067 When activate part place bar grid align state not updated
+
 //===================================================================================
 //Fixed:
 //B055 No road connection to middle of existing segment
