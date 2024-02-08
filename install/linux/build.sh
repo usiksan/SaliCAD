aeec2a3b0886630702ae1560afb8145fb8850ac1
usik <usiksan@mail.ru>
2024-02-08 19:46:10 +0300

    Linux installation acket for 2.7

--- install/linux/build.sh
+++ install/linux/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-~/Qt/Tools/QtInstallerFramework/3.1/bin/archivegen  packages/SaliCAD/data/sali ~/SaliCADinstall/*
+~/Qt/Tools/QtInstallerFramework/4.6/bin/archivegen  packages/SaliCAD/data/sali ~/SaliCADinstall/*
 
-~/Qt/Tools/QtInstallerFramework/3.1/bin/binarycreator -c config/config.xml -p packages SaliCAD_2_1_Linux_x86_64_Install
+~/Qt/Tools/QtInstallerFramework/4.6/bin/binarycreator -c config/config.xml -p packages SaliCAD_2_7_Linux_x86_64_Install
 
 rm packages/SaliCAD/data/sali.7z
